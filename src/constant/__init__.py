30b665902bfb4d6d3a65d91628baeb5172f7ebd7
Ujjwal Dwivedi <ujjwaldwivedi567@gmail.com>
2024-11-17 23:16:28 +0530

    completed all modules

--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -7,7 +7,7 @@ MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URL="mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URI="mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 MODEL_FILE_NAME = "model"
