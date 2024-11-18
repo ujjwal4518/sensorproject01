f7d59b577c502a7893a4f0d09a99965d49d4f515
Ujjwal Dwivedi <ujjwaldwivedi567@gmail.com>
2024-11-18 13:38:22 +0530

    added modules

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,7 +1,7 @@
 import sys
 import os
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 from pymongo import MongoClient
 from zipfile import Path
 from src.constant import *
@@ -16,47 +16,44 @@ class DataIngestionConfig:
     artifact_folder: str = os.path.join(artifact_folder)
 
 
-
 class DataIngestion:
     def __init__(self):
-        self.data_ingestion_config = DataIngestionConfig()
+        self.data_ingestion_config= DataIngestionConfig()
         self.utils = MainUtils()
 
-
     def export_collection_as_dataframe(self,collection_name,db_name):
-        
-        try:
-            mongo_client =  MongoClient(MONGO_DB_URI)
 
-            collection = mongo_client[db_name][collection_name] 
+        try:
+            mongo_client = MongoClient(MONGO_DB_URL)
+
+            collection = mongo_client[db_name][collection_name]
 
             df = pd.DataFrame(list(collection.find()))
 
             if "_id" in df.columns.to_list():
                 df = df.drop(columns=['_id'],axis=1)
-
-            df.replace({"na":np.nan},inplace=True)   
+            
+            df.replace({"na":np.nan},inplace=True)
 
             return df
-        
         except Exception as e:
             raise CustomException(e,sys)
-        
 
     def export_data_into_feature_store_file_path(self)-> pd.DataFrame:
+
         try:
 
             logging.info(f"Exporting data from mongodb")
-            raw_file_path =  self.data_ingestion_config.artifact_folder
+            raw_file_path = self.data_ingestion_config.artifact_folder
 
             os.makedirs(raw_file_path,exist_ok=True)
 
             sensor_data = self.export_collection_as_dataframe(
-                collection_name=MONGO_COLLECTION_NAME,
+                collection_name= MONGO_COLLECTION_NAME,
                 db_name = MONGO_DATABASE_NAME
             )
 
-            logging.info(f"saving exported data into feature store file path : {raw_file_path}")
+            logging.info(f"saving exported data into feature store file path :{raw_file_path}")
 
             feature_store_file_path = os.path.join(raw_file_path,'wafer_fault.csv')
 
@@ -66,20 +63,21 @@ class DataIngestion:
         
         except Exception as e:
             raise CustomException(e,sys)
-        
 
     def initiate_data_ingestion(self) -> Path:
 
-        logging.info("Entered initiated_data_ingestion method of data_integration class")  
+        logging.info("Entered initiated_data_ingestion method of data_integration class")
 
         try:
             feature_store_file_path = self.export_data_into_feature_store_file_path()
 
             logging.info("got the data from mongodb")
 
-            logging.info("exited initiate_data_ingestion methods of data ingestion class")
+            logging.info("exited initiate_data_ingestion methos of data ingestion class")
 
             return feature_store_file_path
-        
         except Exception as e:
             raise CustomException(e,sys) from e
+
+        
+
--- src/constant/__init__.py
+++ src/constant/__init__.py
@@ -7,7 +7,7 @@ MONGO_COLLECTION_NAME = "waferfault"
 
 
 TARGET_COLUMN = "quality"
-MONGO_DB_URI="mongodb+srv://imran:TdPLW9Ad0OzpSSD2@cluster0.fv0lm61.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URI="mongodb+srv://ujju__05:Ujjwaldw12@cluster0.zts4y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 
 MODEL_FILE_NAME = "model"
